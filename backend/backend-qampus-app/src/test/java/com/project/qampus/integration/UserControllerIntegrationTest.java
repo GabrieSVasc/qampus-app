b423ee810fb752b16a7ad3086687d09c29008d35
oalencar <oalencardev@gmail.com>
2026-08-25 14:25:32 -0300

    chore: ajuste de Maintainability issues

--- backend/backend-qampus-app/src/test/java/com/project/qampus/integration/UserControllerIntegrationTest.java
+++ backend/backend-qampus-app/src/test/java/com/project/qampus/integration/UserControllerIntegrationTest.java
@@ -1,8 +1,5 @@
 package com.project.qampus.integration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.qampus.dto.RegisterRequestDTO;
-import com.project.qampus.model.enums.Role;
 import com.project.qampus.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,15 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -32,27 +27,15 @@ class UserControllerIntegrationTest {
     @Autowired
     private UserRepository userRepository;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
     private MockMvc mockMvc;
-    private String validToken;
 
     @BeforeEach
-    void setUp() throws Exception {
+    void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
 
         userRepository.deleteAll();
-
-        RegisterRequestDTO registerDTO = new RegisterRequestDTO("Aluno Teste", "aluno@qampus.com", "senha123", Role.STUDENT);
-        MvcResult result = mockMvc.perform(post("/auth/register")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(registerDTO)))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        String responseContent = result.getResponse().getContentAsString();
-        validToken = objectMapper.readTree(responseContent).get("token").asText();
     }
 
     @Test
